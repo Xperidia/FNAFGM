ec3f92f9898d70a4665ca2b8222754b616a26d28
VictorienXP <VictorienXP@Xperidia.com>
2015-10-24 12:27:38 +0200

    Testing stuff again

---
+++ gamemodes/fnafgm/entities/entities/fnafgm_animatronic/shared.lua
@@ -0,0 +1,70 @@
+AddCSLuaFile()
+
+ENT.Base = "base_entity"
+ENT.Type = "point"
+ENT.PrintName = "FNAFGM Animatronic"
+ENT.Author = "Xperidia"
+
+function ENT:AcceptInput( name, activator, caller, data )
+	
+	if debugmode and IsValid(activator) and IsValid(caller) then print( self:GetName(), name, activator:GetName(), caller:GetName(), data )
+	elseif debugmode and IsValid(activator) then print( self:GetName(), name, activator:GetName(), caller, data )
+	elseif debugmode and IsValid(caller) then print( self:GetName(), name, activator, caller:GetName(), data )
+	elseif debugmode then print( self:GetName(), name, activator, caller, data ) end
+	
+	if name=="Disable" then
+		
+		self.Disabled = true
+		
+		self:SetRenderMode(RENDERMODE_NONE)
+		
+		if debugmode then print(self:GetName().." disabled") end
+		
+	elseif name=="Enable" then
+		
+		self.Disabled = false
+		
+		self:SetRenderMode(RENDERMODE_NORMAL)
+		
+		if debugmode then print(self:GetName().." enabled") end
+		
+	elseif name=="Kill" then
+		
+		self:Remove()
+		
+		if debugmode then print(self:GetName().." removed") end
+		
+	end
+	
+	return true
+	
+end
+
+
+function ENT:KeyValue(k, v)
+	
+	if debugmode then print(k, v) end
+	
+	if k == "Model" then
+		
+		self.Model = v
+		
+	end
+	
+end
+
+function ENT:Initialize()
+	
+	self:SetModel( self.Model )
+	
+end
+
+function ENTITY:Touch( entity )
+	
+	if !self.Disabled and entity:IsPlayer() and entity:Alive() then
+		
+		entity:Kill()
+		
+	end
+	
+end
