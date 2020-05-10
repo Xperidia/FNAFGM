68f66fa3e2475d2b8c1d29a722747a8542376fe8
VictorienXP <VictorienXP@Xperidia.com>
2020-05-10 23:18:07 +0200

    Update fnafgm_keypad.lua

--- gamemodes/fnafgm/entities/entities/fnafgm_keypad.lua
+++ gamemodes/fnafgm/entities/entities/fnafgm_keypad.lua
@@ -51,25 +51,35 @@ end
 function ENT:PasswordInput(password, ply)
 
 	if password == self:GetPassword() then
+
 		self:TriggerOutput("OnCorrectPassword", ply)
+
 		self:SetSkin(1)
+
 		timer.Create("fnafgm_keypad_skin_reset_" .. self:EntIndex(), 1.6, 1,
 		function()
 			if IsValid(self) then
 				self:SetSkin(0)
 			end
 		end)
+
 		self:EmitSound(ok_sound, 120)
+
 	else
+
 		self:TriggerOutput("OnBadPassword", ply)
+
 		self:SetSkin(2)
+
 		timer.Create("fnafgm_keypad_skin_reset_" .. self:EntIndex(), 1.6, 1,
 		function()
 			if IsValid(self) then
 				self:SetSkin(0)
 			end
 		end)
+
 		self:EmitSound(error_sound, 120)
+
 	end
 
 end
